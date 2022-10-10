314e84724b2128aef1dcf815a9ec7e5006b60875
18862002143 <1105925169@qq.com>
2022-10-11 02:11:29 +0800

    second2

---
+++ pages/login/login.js
@@ -0,0 +1,107 @@
+// pages/login/login.js
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+      avatarUrl: '/pages/images/0.png',
+      nickName:'未知用户',
+      touxian:'../images/user.jpg',
+      userInfo:''
+    },
+    onChooseAvatar(e) {
+      const { avatarUrl } = e.detail 
+      this.setData({
+        avatarUrl,
+      })
+    },
+    dianji(){
+   
+      console.log("点击了按按钮")
+      wx.getUserProfile({
+        desc: '编辑会员资料',//获取用户的信息
+         success:res => {//用户成功授权
+         // console.log("成功",res)
+          this.setData({
+            nickName:res.userInfo.nickName,
+            touxian:res.userInfo.avatarUrl
+          })
+          wx.setStorageSync("nkName",this.data.nickName)
+          wx.setStorageSync("avatUrl",this.data.touxian)
+          
+          console.log(wx.getStorageSync('userinfo'))
+        
+          //过1.5s跳转到主页
+          setTimeout(function(){
+            //重定向跳转不了tabbar页面
+          /*  wx.redirectTo({
+             url: '/pages/home/home',
+           }) */
+           wx.switchTab({
+             url:'/pages/home/home',
+           })
+          },1500)
+      },
+      fail:res=>{
+        console.log("失败",res)
+      }
+    })},
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
---
+++ pages/self/self.js
@@ -0,0 +1,70 @@
+// pages/self/self.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+      nickkName:'未知用户',
+      avaUrl:'/pages/images/user.jpg',
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+      this.data.nickkName = wx.getStorageSync("nkName");
+      this.data.avaUrl = wx.getStorageSync("avatUrl");
+      console.log( this.data.nickkName, this.data.avaUrl)
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+      
+     
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+     
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
